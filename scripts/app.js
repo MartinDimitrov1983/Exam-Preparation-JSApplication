6b190305e3d0f9b3a38e6862d8102d8bdf69d4c6
demonnz <demonnz@abv.bg>
2020-09-12 14:08:11 +0300

    some changes

--- scripts/app.js
+++ scripts/app.js
@@ -236,10 +236,7 @@ const app = Sammy('#main', function () {
     this.get('#/delete/:id', deleteHandler);
 
     // this.post('#/catalog/:id', () => false);
-    // this.get('#/catalog/:id', commentsHandler);
-    this.post('#/catalog/:id', () => false);
-    this.get('#/catalog/:id', commentsHandler);
-    
+    // this.get('#/catalog/:id', commentsHandler);    
 
     // this.get('#/details/:id', detailsHandler);
 
